31c797ddaada3f03cb0228d7ba99c1d2cb020487
mahomedalid <mahomedalidla@gmail.com>
2021-12-11 02:02:02 -0700

    Funcionalidad correcta de Maps

--- app/src/main/java/mx/edu/tepic/lamd_u5_eje1_geomapatec/MainActivity.kt
+++ app/src/main/java/mx/edu/tepic/lamd_u5_eje1_geomapatec/MainActivity.kt
@@ -2,7 +2,9 @@ package mx.edu.tepic.lamd_u5_eje1_geomapatec
 
 
 import android.Manifest
+import android.annotation.SuppressLint
 import android.content.Context
+import android.content.Intent
 import android.content.pm.PackageManager
 import android.location.Location
 import android.location.LocationListener
@@ -119,7 +121,7 @@ class MainActivity : AppCompatActivity() {
             AlertDialog.Builder(this).setTitle("Atención")
                 .setMessage("¿Qué desea hacer con\n${dataLista[position]}?")
                 .setPositiveButton("Ver en el mapa"){d,w->
-                    //abrirMapa(listaID[position])
+                    abrirMapa(listaID[position])
                 }
                 .setNeutralButton("Cancelar"){d,w->}
                 .show()
@@ -127,36 +129,87 @@ class MainActivity : AppCompatActivity() {
     }//cargar Lista
 /////////////
 
-    private fun miUbicacion() {
-        if (ActivityCompat.checkSelfPermission(
-                this,
-                Manifest.permission.ACCESS_FINE_LOCATION
-            ) != PackageManager.PERMISSION_GRANTED && ActivityCompat.checkSelfPermission(
-                this,
-                Manifest.permission.ACCESS_COARSE_LOCATION
-            ) != PackageManager.PERMISSION_GRANTED
-        ) {
-            return
-        }
-        LocationServices.getFusedLocationProviderClient(this)
-         .lastLocation.addOnSuccessListener {
-            var geoPosicion = GeoPoint(it.latitude,it.longitude)
-             textView2.setText("${it.latitude},${it.longitude}")
-             for (item in posicion){
+    fun cargarListaBuscador(edificio:String){
+        baseRemota.collection("tecnologico")
+            .whereEqualTo("nombre",edificio)
+            .addSnapshotListener { querySnapshot, firebaseFirestoreException ->
+                if (firebaseFirestoreException != null) {
+                    mensaje("Error: " + firebaseFirestoreException.message)
+                    return@addSnapshotListener
+                }//if
+                var resultado = ""
+                var listaCadena=""
+                posicion.clear()
+                dataLista.clear()
+                listaID.clear()
+                for (document in querySnapshot!!) {
+                    var data = Data()
+                    data.nombre = document.getString("nombre").toString()
+                    data.posicion1 = document.getGeoPoint("posicion1")!!
+                    data.posicion2 = document.getGeoPoint("posicion2")!!
 
-                 if (item.estoyEn(geoPosicion)){
-                     AlertDialog.Builder(this)
-                         .setMessage("USTED SE ENCUENTRA EN "+item.nombre)
-                         .setTitle("ATENCION")
-                         .setPositiveButton("OK"){p,q->}
-                         .show()
-                 }
-             }
-         }.addOnFailureListener {
-             textView2.setText("ERROR AL OBTENER UBICACION")
-         }
+                    resultado += data.toString() + "\n" + "\n"
+                    listaCadena=data.toString()
+                    posicion.add(data)
+                    dataLista.add(listaCadena)
+                    listaID.add(document.id)
+                }//for
+                if (dataLista.size == 0) {
+                    dataLista.add("No hay data")
+                }//if
+                var adaptador =
+                    ArrayAdapter<String>(this, android.R.layout.simple_list_item_1, dataLista)
+                lista.adapter = adaptador
+            }//add snap
+        lista.setOnItemClickListener { parent, view, position, id ->
+            if (listaID.size == 0) {
+                return@setOnItemClickListener
+            }//if
+            AlertDialog.Builder(this).setTitle("Atención")
+                .setMessage("¿Qué desea hacer con\n${dataLista[position]}?")
+                .setPositiveButton("Ver en el mapa"){d,w->
+                    abrirMapa(listaID[position])
+                }
+                .setNeutralButton("Cancelar"){d,w->}
+                .show()
+        }//lista
+    }//cargar Lista
+
+    fun abrirMapa(idLugar:String){
+        baseRemota.collection("tecnologico")
+            .document(idLugar)
+            .get()
+            .addOnSuccessListener {
+                var v= Intent(this,MapsActivity::class.java)
+                v.putExtra("id",idLugar)
+                v.putExtra("data",it.getString("nombre"))
+                startActivity(v)
+            }
+            .addOnFailureListener { Toast.makeText(this, "Error no hay conexión de red", Toast.LENGTH_LONG)
+                .show() }
     }
-}
+
+    @SuppressLint("MissingPermission")
+    private fun miUbicacion() {
+        LocationServices.getFusedLocationProviderClient(this)
+            .lastLocation.addOnSuccessListener {
+                var geoPosicion = GeoPoint(it.latitude, it.longitude)
+                textView2.setText("${it.latitude}, ${it.longitude}")
+                for (item in posicion) {
+                    if (item.estoyEn(geoPosicion)) {
+                        AlertDialog.Builder(this)
+                            .setMessage("Usted se encuentra en: " + item.nombre)
+                            .setTitle("ATENCION")
+                            .setPositiveButton("OK") { p, q ->  }
+                            .show()
+                    }//if
+                }//for
+            }//success
+            .addOnFailureListener {
+                textView2.setText("ERROR AL OBTENER UBICACIÓN")
+            }//fail
+    }
+}//Class
 
 
 
--- app/src/main/java/mx/edu/tepic/lamd_u5_eje1_geomapatec/MapsActivity.kt
+++ app/src/main/java/mx/edu/tepic/lamd_u5_eje1_geomapatec/MapsActivity.kt
@@ -1,7 +1,13 @@
 package mx.edu.tepic.lamd_u5_eje1_geomapatec
 
+import android.annotation.SuppressLint
+import android.content.pm.PackageManager
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.widget.Toast
+import androidx.core.app.ActivityCompat
+import com.google.android.gms.location.FusedLocationProviderClient
+import com.google.android.gms.location.LocationServices
 
 import com.google.android.gms.maps.CameraUpdateFactory
 import com.google.android.gms.maps.GoogleMap
@@ -9,23 +15,37 @@ import com.google.android.gms.maps.OnMapReadyCallback
 import com.google.android.gms.maps.SupportMapFragment
 import com.google.android.gms.maps.model.LatLng
 import com.google.android.gms.maps.model.MarkerOptions
+import com.google.firebase.firestore.FirebaseFirestore
 import mx.edu.tepic.lamd_u5_eje1_geomapatec.databinding.ActivityMapsBinding
 
 class MapsActivity : AppCompatActivity(), OnMapReadyCallback {
 
     private lateinit var mMap: GoogleMap
-    private lateinit var binding: ActivityMapsBinding
+    //private lateinit var binding: ActivityMapsBinding
+    var baseRemota = FirebaseFirestore.getInstance()
+    var marcadores=ArrayList<Data>()
+    var lat=0.0
+    var long=0.0
+    var edificio=""
+    lateinit var ubicacionCliente: FusedLocationProviderClient
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_maps)
 
-        binding = ActivityMapsBinding.inflate(layoutInflater)
-        setContentView(binding.root)
+        var extra = intent.extras
+        edificio=extra!!.get("data").toString()
 
         // Obtain the SupportMapFragment and get notified when the map is ready to be used.
         val mapFragment = supportFragmentManager
             .findFragmentById(R.id.map) as SupportMapFragment
         mapFragment.getMapAsync(this)
+        if(ActivityCompat.checkSelfPermission(this,android.Manifest.permission.ACCESS_FINE_LOCATION)!= PackageManager.PERMISSION_GRANTED){
+            ActivityCompat.requestPermissions(this, arrayOf(android.Manifest.permission.ACCESS_FINE_LOCATION),1)
+
+        }
+        ubicacionCliente= LocationServices.getFusedLocationProviderClient(this)
+
     }
 
     /**
@@ -37,12 +57,46 @@ class MapsActivity : AppCompatActivity(), OnMapReadyCallback {
      * it inside the SupportMapFragment. This method will only be triggered once the user has
      * installed Google Play services and returned to the app.
      */
+    @SuppressLint("MissingPermission")
     override fun onMapReady(googleMap: GoogleMap) {
-        mMap = googleMap
+        baseRemota.collection("tecnologico")
+            .whereEqualTo("nombre",edificio)
+            .addSnapshotListener { querySnapshot, firebaseFirestoreException ->
+                if (firebaseFirestoreException != null) {
+                    setTitle("Error: " + firebaseFirestoreException.message)
+                    return@addSnapshotListener
+                }//if
+                var resultado = ""
+                marcadores.clear()
+                for (document in querySnapshot!!) {
+                    var data = Data()
 
-        // Add a marker in Sydney and move the camera
-        val sydney = LatLng(-34.0, 151.0)
-        mMap.addMarker(MarkerOptions().position(sydney).title("Marker in Sydney"))
-        mMap.moveCamera(CameraUpdateFactory.newLatLng(sydney))
-    }
+                    data.nombre = document.getString("nombre").toString()
+                    data.posicion1 = document.getGeoPoint("posicion1")!!
+                    data.posicion2 = document.getGeoPoint("posicion2")!!
+                    resultado += data.toString() + "\n" + "\n"
+                    marcadores.add(data)
+                    lat=data.posicion1.latitude
+                    long=data.posicion1.longitude
+                }
+                Toast.makeText(this, resultado, Toast.LENGTH_LONG).show()
+                mMap = googleMap
+                setTitle(edificio)
+
+                // Add a marker in Sydney and move the camera
+                val sydney = LatLng(lat,long)
+                mMap.addMarker(MarkerOptions().position(sydney).title(edificio))
+                mMap.moveCamera(CameraUpdateFactory.newLatLng(sydney))
+                mMap.uiSettings.isZoomControlsEnabled=true
+                mMap.uiSettings.isMyLocationButtonEnabled=true
+                mMap.isMyLocationEnabled=true
+                ubicacionCliente.lastLocation.addOnSuccessListener {
+
+                    if(it!=null){
+                        val posMark=LatLng(lat,long)
+                        mMap.animateCamera(CameraUpdateFactory.newLatLngZoom(posMark,18f))
+                    }
+                }//addonsucc
+            }//add snap
+    }//onMap
 }
